aaeb4ae9aa4fac744354a4e1515bd7771fb59d53
Razaan <razaanreza0705@gmail.com>
2026-02-10 21:50:36 +0600

    fixed design errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,26 @@
-import { SplashScreen, Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
-import { useEffect} from "react";
- 
+import { Stack, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import './global.css'
+
+
+SplashScreen.preventAutoHideAsync();
+
 export default function Layout() {
-  const [fontsLoaded, error] = useFonts({
-    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
-    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
-    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
-    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
-    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+  const [loaded, error] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
   });
+
   useEffect(() => {
-    if(error) throw error;
-    if(fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded, error]);
+    if (error) throw error;
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded, error]);
+
+  if (!loaded) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
