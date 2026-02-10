31cd2a28598b0cadd347638e69912548501d5b18
Razaan <razaanreza0705@gmail.com>
2026-02-10 19:51:35 +0600

    custom fonts added

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
-import "./global.css"
-import { Text, View } from "react-native";
- 
+import { Text, View } from 'react-native'
+import './global.css'
+
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text
+        style={{ fontFamily: 'QuickSand-Bold', fontSize: 20, color: 'red' }}
+      >
         Welcome to Nativewind Razaan!
       </Text>
     </View>
-  );
+  )
 }
